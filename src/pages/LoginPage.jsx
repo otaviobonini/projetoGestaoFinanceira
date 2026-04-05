f454b91ce91481a3a46f203074a92fee05bfa98b
otaviobonini <otgbonini@gmail.com>
2026-04-05 16:33:41 -0300

    style: revamped the register and login page style

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -44,52 +44,56 @@ export default function LoginPage() {
 
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <form
-        className="min-w-fit  bg-white p-6 rounded-md shadow-md flex flex-col"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h1 className="text-center mb-4 font-bold  uppercase text-2xl">
-          LOGIN
-        </h1>
-        <label htmlFor="email">Email</label>
-        <input
-          className="mb-3 p-2 rounded border"
-          type="email"
-          {...register("email")}
-          placeholder="Insira seu email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          className="mb-3 p-2 rounded border"
-          type="password"
-          {...register("password")}
-          placeholder="Insira sua senha"
-        />
-
-        <button
-          type="submit"
-          disabled={isSubmitting}
-          className="bg-green-400 rounded-md p-2 text-white mb-4"
+      <div className="flex rounded-lg overflow-hidden shadow-lg">
+        <div className="bg-blue-500 text-white p-10 flex flex-col justify-center max-w-sm">
+          <h1 className="text-3xl font-bold mb-2">Bem-vindo de volta!</h1>
+          <p className="opacity-90">Faça seu acesso com uma conta existente</p>
+        </div>
+        <form
+          className="min-w-fit  bg-white p-6  flex flex-col"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {isSubmitting ? "Enviando" : "Entrar"}
-        </button>
-        <NavLink to={"/register"}>
-          Não tem uma conta?{" "}
-          <span className="text-blue-500 font-bold hover:cursor-pointer">
-            Cadastre-se
-          </span>
-        </NavLink>
-        {error && (
-          <p className="text-red-500 mt-3 capitalize text-sm text-center">
-            Error: {error}
-          </p>
-        )}
-        {success && (
-          <p className="text-green-500 mt-3 capitalize text-sm text-center">
-            Login feito com sucesso
-          </p>
-        )}
-      </form>
+          <h1 className="text-center mb-4 uppercase text-2xl">LOGIN</h1>
+          <label htmlFor="email">Email</label>
+          <input
+            className="mb-3 p-2 rounded-2xl border focus:outline-none focus:ring-blue-400 focus:ring-1"
+            type="email"
+            {...register("email")}
+            placeholder="Insira seu email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            className="mb-3 p-2  border rounded-2xl focus:outline-none focus:ring-blue-400 focus:ring-1"
+            type="password"
+            {...register("password")}
+            placeholder="Insira sua senha"
+          />
+
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="bg-green-400 rounded-2xl p-2 text-white mb-4 hover:bg-green-500 transition duration-300"
+          >
+            {isSubmitting ? "Enviando" : "Entrar"}
+          </button>
+          <NavLink to={"/register"}>
+            Não tem uma conta?{" "}
+            <span className="text-blue-500 font-bold hover:cursor-pointer">
+              Cadastre-se
+            </span>
+          </NavLink>
+          {error && (
+            <p className="text-red-500 mt-3 capitalize text-sm text-center">
+              Error: {error}
+            </p>
+          )}
+          {success && (
+            <p className="text-green-500 mt-3 capitalize text-sm text-center">
+              Login feito com sucesso
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { NavLink, useNavigate } from "react-router-dom";
 
-export default function LoginPage() {
+export default function RegisterPage() {
   const { register, handleSubmit } = useForm();
   const [error, setError] = useState(null);
   const [success, setSucess] = useState(false);
@@ -41,60 +41,64 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <form
-        className="min-w-fit  bg-white p-6 rounded-md shadow-md flex flex-col"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h1 className="text-center mb-4 font-bold  uppercase text-2xl">
-          REGISTER
-        </h1>
-        <label htmlFor="email">Name</label>
-        <input
-          className="mb-3 p-2 rounded border"
-          type="text"
-          {...register("name")}
-          placeholder="Insira seu nome"
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          className="mb-3 p-2 rounded border"
-          type="email"
-          {...register("email")}
-          placeholder="Insira seu email"
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          className="mb-3 p-2 rounded border"
-          type="password"
-          {...register("password")}
-          placeholder="Insira sua senha"
-        />
-
-        <button
-          type="submit"
-          disabled={isSubmiting}
-          className="bg-green-400 rounded-md p-2 text-white mb-4"
+    <div className="bg-gray-100 min-h-screen flex  items-center justify-center">
+      <div className="flex overflow-hidden rounded-xl  shadow-md">
+        <div className="bg-blue-500 text-white p-10 flex flex-col justify-center max-w-sm">
+          <h1 className="text-3xl text-center font-bold mb-2">Bem-vindo!</h1>
+          <p className="opacity-90">Crie sua conta para continuar</p>
+        </div>
+        <form
+          className="min-w-fit  bg-white p-12  flex flex-col"
+          onSubmit={handleSubmit(onSubmit)}
         >
-          {isSubmiting ? "Registrando..." : "Registre-se"}
-        </button>
-        <NavLink to={"/login"}>
-          Já tem uma conta?{" "}
-          <span className="text-blue-500 font-bold hover:cursor-pointer">
-            Entre aqui
-          </span>
-        </NavLink>
-        {error && (
-          <p className="text-red-500 mt-3 capitalize text-sm text-center">
-            Erro: {error}
-          </p>
-        )}
-        {success && (
-          <p className="text-green-500 mt-3 capitalize text-sm text-center">
-            Registro feito com sucesso
-          </p>
-        )}
-      </form>
+          <h1 className="text-center mb-4  uppercase text-2xl">REGISTER</h1>
+          <label htmlFor="email">Name</label>
+          <input
+            className="mb-3 p-2 rounded-2xl border focus:outline-none focus:ring-blue-400 focus:ring-1"
+            type="text"
+            {...register("name")}
+            placeholder="Insira seu nome"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            className="mb-3 p-2 rounded-2xl border focus:outline-none focus:ring-blue-400 focus:ring-1"
+            type="email"
+            {...register("email")}
+            placeholder="Insira seu email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            className="mb-3 p-2 rounded-2xl border focus:outline-none focus:ring-blue-400 focus:ring-1"
+            type="password"
+            {...register("password")}
+            placeholder="Insira sua senha"
+          />
+
+          <button
+            type="submit"
+            disabled={isSubmiting}
+            className="bg-green-400 rounded-2xl p-2 text-white mb-4 hover:bg-green-500 transition duration-300"
+          >
+            {isSubmiting ? "Registrando..." : "Registre-se"}
+          </button>
+          <NavLink to={"/login"}>
+            Já tem uma conta?{" "}
+            <span className="text-blue-500 font-bold hover:cursor-pointer">
+              Entre aqui
+            </span>
+          </NavLink>
+          {error && (
+            <p className="text-red-500 mt-3 capitalize text-sm text-center">
+              Erro: {error}
+            </p>
+          )}
+          {success && (
+            <p className="text-green-500 mt-3 capitalize text-sm text-center">
+              Registro feito com sucesso
+            </p>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
